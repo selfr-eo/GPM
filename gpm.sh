05a9e2978c672736c6e3b07223b946ed22f91b13
selfr-eo <sarah.e.franze@gmail.com>
2025-08-29 15:32:32 +0200

    update gpm script

--- gpm.sh
+++ gpm.sh
@@ -22,46 +22,107 @@
 # This downloads the full day's data from 2 days from the date the script is run.
 #
 # Date created: 25-10-2023
+
+# UPDATES - SAFR (29-08-2025)
+# - directory now a command line input
+# - version now a variable input
+# - sequenceID now a variable input (more robust than index)
+# - wget fields updated to be compatible running with git bash shell on windows computer
 #
-# To run the script ./gpm.sh
+# To run the script ./gpm.sh $year $directory
 
 # The year to download:
 yr=$1
-              
+directory=$2
+
 # The URL, base directory, and step size
-url=https://gpm1.gesdisc.eosdis.nasa.gov/opendap/GPM_L3/GPM_3IMERGHH.07
-directory="/g/data/ia39/aus-ref-clim-data-nci/gpm/data/tmp"
+#url=https://gpm1.gesdisc.eosdis.nasa.gov/opendap/GPM_L3/GPM_3IMERGHH.07
+url=https://gpm1.gesdisc.eosdis.nasa.gov/opendap/hyrax/GPM_L3/GPM_3IMERGHH.07
+#directory="/g/data/ia39/aus-ref-clim-data-nci/gpm/data/tmp"
 step_size=$((30*60))
               
 # Create time strings for the 30min data in the format hhmmss
 # i.e. 0000000 - 003000 is 00:00:00 - 00:30:00
 declare -a time_pairs
+declare -a sequenceIDs
+
 for ((time=$(date -d"today 00:00:00" +%s); time<$(date -d"today 23:59:59" +%s); time+=$step_size)); do
   start_time=$(date -d"@$time" +%H%M%S)
   end_time=$(date -d"@$((time+step_size-1))" +%H%M%S)
   time_pairs+=("$start_time,$end_time")
+
 done
-              
+
+# compute sequence IDs
+for ((seq=0; seq<=1410; seq+=30)); do
+  sequenceIDs+=("$(printf "%04d" $seq)")
+done
+
 # Function to check in the year being used is a leap or not
 is_leap_year() {
   (( !(yr % 4) && (yr % 100) || !(yr % 400) ))
   }
               
-# Function to use wget to download the data
+
 download_file() {
-  local day=$1
-  local start_time=$2
-  local end_time=$3
-  local index=$(printf "%04d" $((30*($4-1))))
-  local dt=$(date -d "01/01/$yr +$day days -1 day" "+%m%d")
-  wget --load-cookies ~/.urs_cookies --save-cookies ~/.urs_cookies --keep-session-cookies -c -nc $url/$yr/$day/3B-HHR.MS.MRG.3IMERG.$yr$dt-S${start_time}-E${end_time}.$index.V07A.HDF5.nc4 >> $yr_$day.log 2>&1
-  }
+  #local yr=$1           # Year (e.g., 2023)
+  local day=$1          # Day of year (1-365)
+  local start_time=$2   # Start time string (e.g., S013000)
+  local end_time=$3     # End time string (e.g., E015959)
+  local sequenceID=$4   # Sequence indicator (e.g., 0000, 0030, 0060)
+  local version=$5      # Version string (e.g., V07B)
+
+  # Format day-of-year as 3-digit for folder
+  local doy=$(printf "%03d" $day)
+
+  # Construct date string for filename (mmdd)
+  local dt=$(date -d "01/01/$yr +$((day-1)) days" "+%m%d")
+  
+  # Construct filename
+  local file="3B-HHR.MS.MRG.3IMERG.${yr}${dt}-${start_time}-${end_time}.${sequenceID}.${version}.HDF5"
+  local file_save=${file}.nc4
+  
+  # Construct full URL
+  local url="https://gpm1.gesdisc.eosdis.nasa.gov/opendap/hyrax/GPM_L3/GPM_3IMERGHH.07/${yr}/${doy}/${file}.dap.nc4"
+  
+  # Use wget to download with Earthdata authentication
+  wget --load-cookies ~/.urs_cookies \
+       --save-cookies ~/.urs_cookies \
+       --keep-session-cookies \
+       --no-check-certificate \
+       --auth-no-challenge=on \
+       -O "$file_save" \
+       "$url" >> "$yr_$day.log" 2>&1
+
+  echo "$(date +'%Y-%m-%d %H:%M:%S') Downloading $file" | tee -a "$yr_$day.log"
+}
+
+# download_file() {
+#   local day=$1
+#   local start_time=$2
+#   local end_time=$3
+#   local sID=$4
+#   local version=$5
+#   local dt=$(date -d "01/01/$yr +$day days -1 day" "+%m%d")
+#   local file="3B-HHR.MS.MRG.3IMERG.${yr}${dt}-${start_time}-${end_time}.${sID}.${version}.HDF5"
+#   local file_save=${file}.nc4
+#   local url="https://gpm1.gesdisc.eosdis.nasa.gov/opendap/hyrax/GPM_L3/GPM_3IMERGHH.07/${yr}/${doy}/${file}.dap.nc4"
+#   wget --load-cookies ~/.urs_cookies \
+#        --save-cookies ~/.urs_cookies \
+#        --keep-session-cookies \
+#        --no-check-certificate \
+#        --auth-no-challenge=on \
+#        -O "$file_save" \
+#        "$url" >> "$yr_$day.log" 2>&1
+#   #wget --load-cookies ~/.urs_cookies --save-cookies ~/.urs_cookies --keep-session-cookies --no-check-certificate --auth-no-challenge=on -O "$file_save" "$url" >> "$yr_$day.log" 2>&1
+#   echo "$(date +'%Y-%m-%d %H:%M:%S') Downloading $file" | tee -a "$yr_$day.log"
+#   }
               
 # Loop either 365 or 366 depending on leap year
 total_days=$(is_leap_year "$yr" && echo "366" || echo "365")
               
 # Main loop to combine everything:
-for ((i=200; i<=$total_days; i++)); do
+for ((i=1; i<=$total_days; i++)); do
   # change day from 1 to 001 to match url directory:
   ii=$(printf "%03d" $i)
   # Check if the directory exists, create it if not, and then cd into it:
@@ -77,8 +138,7 @@ for ((i=200; i<=$total_days; i++)); do
   echo "Downloading data for day $ii in $yr"
   for ((j=0; j<${#time_pairs[@]}; j++)); do
     IFS="," read -ra time_pair <<< "${time_pairs[j]}"
-    download_file "$ii" "${time_pair[0]}" "${time_pair[1]}" "$j+1"
-    #echo "${time_pair[1]}"
+    download_file "$ii" "S${time_pair[0]}" "E${time_pair[1]}" "${sequenceIDs[j]}" "V07B" #  day, start time, end time, sequence ID, version
   done
 done
 
